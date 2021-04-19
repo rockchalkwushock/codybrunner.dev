da7aeb10f47557b39424c74379f6be29911d0c06
Cody Brunner <rockchalkwushock@icloud.com>
2021-04-19 08:44:17 -0500

    feat(hooks): adds useDimensions hook

---
+++ hooks/useDimensions.ts
@@ -0,0 +1,23 @@
+import * as React from 'react'
+
+type UseDimensions = (
+  ref: React.RefObject<HTMLElement>
+) => {
+  height: number
+  width: number
+}
+
+export const useDimensions: UseDimensions = ref => {
+  const dimensions = React.useRef({ height: 0, width: 0 })
+
+  React.useEffect(() => {
+    if (!ref.current) {
+      return
+    }
+    dimensions.current.height = ref.current.offsetHeight
+    dimensions.current.width = ref.current.offsetWidth
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return dimensions.current
+}
