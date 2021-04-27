d55b592fced3965dc5ac8d2b1ca568aba3f5cc3a
Cody Brunner <rockchalkwushock@icloud.com>
2021-04-27 11:22:42 -0500

    feat(pages): integrates react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { AppProps } from 'next/app'
 import { AnimatePresence } from 'framer-motion'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 import '../styles/global.scss'
 import { AppNav } from '@components/AppNav'
@@ -11,22 +13,32 @@ import { OpenToWorkBanner } from '@components/OpenToWorkBanner'
 interface Props extends AppProps {}
 
 const App: React.FC<Props> = ({ Component, pageProps, router }) => {
+  const queryClientRef = React.useRef<QueryClient>()
+
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient()
+  }
+
   const handleExit = React.useCallback(
     () => typeof window !== 'undefined' && window.scrollTo(0, 0),
     []
   )
+
   return (
-    <div className="container flex flex-col flex-grow items-center justify-center mx-auto relative w-full">
-      <OpenToWorkBanner />
-      <AppNav />
-      <main className="flex-grow grid grid-cols-1 h-full mt-10 w-full md:gap-8 md:grid-cols-5 md:mt-0">
-        <Aside />
-        <AnimatePresence exitBeforeEnter onExitComplete={handleExit}>
-          <Component {...pageProps} key={router.asPath} />
-        </AnimatePresence>
-      </main>
-      <Footer />
-    </div>
+    <QueryClientProvider client={queryClientRef.current}>
+      <div className="container flex flex-col flex-grow items-center justify-center mx-auto relative w-full">
+        <OpenToWorkBanner />
+        <AppNav />
+        <main className="flex-grow grid grid-cols-1 h-full mt-10 w-full md:gap-8 md:grid-cols-5 md:mt-0">
+          <Aside />
+          <AnimatePresence exitBeforeEnter onExitComplete={handleExit}>
+            <Component {...pageProps} key={router.asPath} />
+          </AnimatePresence>
+        </main>
+        <Footer />
+      </div>
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
   )
 }
 
