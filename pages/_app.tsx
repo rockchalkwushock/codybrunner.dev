d314f5b89c1e0d7407506bc92ee3f9fbdbeecf7a
Cody Brunner <rockchalkwushock@icloud.com>
2021-05-04 07:34:59 -0500

    feat(pages): adds ThemeProvider to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { AppProps } from 'next/app'
 import { AnimatePresence } from 'framer-motion'
+import { ThemeProvider } from 'next-themes'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
@@ -30,17 +31,24 @@ const App: React.FC<Props> = ({ Component, pageProps, router }) => {
 
   return (
     <QueryClientProvider client={queryClientRef.current}>
-      <OpenToWorkBanner />
-      <div className="container flex flex-col flex-grow items-center justify-center mx-auto relative space-y-8 w-full">
-        <AppNav />
-        <main className="flex-grow grid grid-cols-1 h-full mt-10 w-full md:gap-8 md:grid-cols-5 md:mt-0">
-          <Aside />
-          <AnimatePresence exitBeforeEnter onExitComplete={handleExit}>
-            <Component {...pageProps} key={router.asPath} />
-          </AnimatePresence>
-        </main>
-        <Footer />
-      </div>
+      <ThemeProvider
+        // This needs to match what is being used in tailwind.config.js
+        attribute="class"
+        // The key that will put in localStorage.
+        storageKey="codybrunner.dev-theme"
+      >
+        <OpenToWorkBanner />
+        <div className="container flex flex-col flex-grow items-center justify-center mx-auto relative space-y-8 w-full">
+          <AppNav />
+          <main className="flex-grow grid grid-cols-1 h-full mt-10 w-full md:gap-8 md:grid-cols-5 md:mt-0">
+            <Aside />
+            <AnimatePresence exitBeforeEnter onExitComplete={handleExit}>
+              <Component {...pageProps} key={router.asPath} />
+            </AnimatePresence>
+          </main>
+          <Footer />
+        </div>
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen />
     </QueryClientProvider>
   )
