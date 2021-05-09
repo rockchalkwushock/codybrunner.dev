620ecf8229b0f972d98e7869e79b76536de06535
Cody Brunner <rockchalkwushock@icloud.com>
2021-05-09 17:00:26 -0500

    feat(root): adds preact and updates next.config to reduce bundle size in production

--- next.config.js
+++ next.config.js
@@ -23,6 +23,15 @@ module.exports = withTM({
       }
     }
 
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+
     return config
   },
 })
