573b3999c7ad9a2f267c8e7a4e47de101f106ee5
Cody Brunner <rockchalkwushock@icloud.com>
2021-05-18 18:07:03 -0500

    fix(github): fixes typescript error

--- pages/api/github/graphql.ts
+++ pages/api/github/graphql.ts
@@ -5,8 +5,8 @@ import { getGitHub } from '@lib/github'
 
 const handler = async (_: NextApiRequest, res: NextApiResponse<GitHub>) => {
   try {
-    const { contributions, pullRequests } = await getGitHub()
-    return res.status(200).json({ contributions, pullRequests })
+    const { pullRequests } = await getGitHub()
+    return res.status(200).json({ pullRequests })
   } catch (error) {
     throw new Error(error)
   }
