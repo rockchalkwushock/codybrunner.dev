84a7f620a9be24eb8c43450943d544bb418e2c91
Cody Brunner <rockchalkwushock@icloud.com>
2021-07-28 12:20:45 -0500

    feat(pages): adds query for all ghost-cms posts to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,27 @@
 import * as React from 'react'
 import { GetStaticProps } from 'next'
+import { PostsOrPages } from '@tryghost/content-api'
+import NextLink from 'next/link'
 
 import { AnimatedPage, PageMetaData } from '@components/AnimatedPage'
 import { ArchivedPost } from '@components/ArchivedPost'
 import { PostListItem } from '@components/PostListItem'
+import { getGhostPosts } from '@lib/ghost-cms'
 import { Post } from '@interfaces/blog'
 import {
   createArchivedPostMap,
   filterPosts,
   getAllPostsFrontMatter,
 } from '@utils/mdx'
+import { formatDateTime } from '@utils/dateTime'
 
 interface Props {
   archive: Record<string, Array<Post>>
+  ghostPosts: PostsOrPages
   posts: Array<Post>
 }
 
-const Home: React.FC<Props> = ({ archive, posts }) => {
+const Home: React.FC<Props> = ({ archive, ghostPosts, posts }) => {
   const pageMetaData: PageMetaData = {
     description: 'My stretch of pipe in the world wide inter-tubes.',
     title: 'codybrunner.dev | Home',
@@ -24,6 +29,30 @@ const Home: React.FC<Props> = ({ archive, posts }) => {
   }
   return (
     <AnimatedPage pageMetaData={pageMetaData}>
+      <h1 className="font-medium mb-4 text-3xl underline">Ghost CMS Posts</h1>
+      <ul className="flex flex-col mb-8 overflow-y-scroll p-2 space-y-6 lg:space-y-4">
+        {ghostPosts.map(post => (
+          <li key={post.id}>
+            {console.log(post)}
+            <NextLink
+              href={{
+                pathname: '/posts/[...slug]',
+                query: { slug: post.slug },
+              }}
+              passHref
+              scroll={false}
+            >
+              <a>
+                <h2>{post.title}</h2>
+                <span>
+                  {post.published_at &&
+                    formatDateTime(post.published_at, 'full-date-localized')}
+                </span>
+              </a>
+            </NextLink>
+          </li>
+        ))}
+      </ul>
       <h1 className="font-medium mb-4 text-3xl underline">Latest Posts</h1>
       <ul className="flex flex-col mb-8 overflow-y-scroll p-2 space-y-6 lg:space-y-4">
         {posts.map(({ frontMatter }) => (
@@ -56,12 +85,14 @@ const Home: React.FC<Props> = ({ archive, posts }) => {
 export const getStaticProps: GetStaticProps<Props> = async () => {
   try {
     const posts = await getAllPostsFrontMatter()
+    const ghostPosts = await getGhostPosts()
 
     return {
       props: {
         archive: createArchivedPostMap(
           filterPosts(posts, p => p.frontMatter.archived)
         ),
+        ghostPosts,
         // All posts for current year.
         posts: filterPosts(posts, p => !p.frontMatter.archived),
       },
