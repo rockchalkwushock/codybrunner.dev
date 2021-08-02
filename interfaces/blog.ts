7624c91c898ea40a11f1265fb335f34a5b67cd5f
Cody Brunner <rockchalkwushock@icloud.com>
2021-08-01 22:05:28 -0500

    feat(root): hodge podge commit

--- interfaces/blog.ts
+++ interfaces/blog.ts
@@ -1,22 +1,36 @@
+import { Pagination } from '@tryghost/content-api'
 import { Maybe } from './helpers'
 
-interface FrontMatter {
-  archived: boolean
-  createdAt: string
-  description: string
-  keywords: Array<string>
-  published: boolean
-  readingTime: string
-  slug: string
-  tags: Array<string>
-  title: string
-  updatedAt?: string
-  wordCount: number
+interface Author {
+  id: string
+  image: Maybe<string>
+  name: string
+}
+
+interface Tag {
+  count: { posts: number }
+  name: string
+  url: string
 }
 
 export interface Post {
-  frontMatter: FrontMatter
-  nextPost?: Maybe<string>
-  previousPost?: Maybe<string>
+  author: Author
+  createdAt: string
+  excerpt: string
+  featured?: boolean
+  id: string
+  image?: Maybe<string>
+  publishedAt: string
+  readingTime: string
+  slug: string
   source: string
+  tags: Array<Tag>
+  title: string
+  updatedAt: string
+  url: string
+}
+
+export interface GetPostsResponse {
+  pagination: Pagination
+  posts: Array<Post>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,14 @@
 import * as React from 'react'
 import { GetStaticProps } from 'next'
-import { PostsOrPages } from '@tryghost/content-api'
-import NextLink from 'next/link'
 
 import { AnimatedPage, PageMetaData } from '@components/AnimatedPage'
-import { ArchivedPost } from '@components/ArchivedPost'
-import { PostListItem } from '@components/PostListItem'
-import { getGhostPosts } from '@lib/ghost-cms'
-import { Post } from '@interfaces/blog'
-import {
-  createArchivedPostMap,
-  filterPosts,
-  getAllPostsFrontMatter,
-} from '@utils/mdx'
-import { formatDateTime } from '@utils/dateTime'
+import { PostLink } from '@components/PostLink'
+import { GetPostsResponse } from '@interfaces/blog'
+import { getPosts } from '@lib/ghost-cms'
 
-interface Props {
-  archive: Record<string, Array<Post>>
-  ghostPosts: PostsOrPages
-  posts: Array<Post>
-}
+interface Props extends GetPostsResponse {}
 
-const Home: React.FC<Props> = ({ archive, ghostPosts, posts }) => {
+const Home: React.FC<Props> = ({ posts }) => {
   const pageMetaData: PageMetaData = {
     description: 'My stretch of pipe in the world wide inter-tubes.',
     title: 'codybrunner.dev | Home',
@@ -31,50 +18,15 @@ const Home: React.FC<Props> = ({ archive, ghostPosts, posts }) => {
     <AnimatedPage pageMetaData={pageMetaData}>
       <h1 className="font-medium mb-4 text-3xl underline">Ghost CMS Posts</h1>
       <ul className="flex flex-col mb-8 overflow-y-scroll p-2 space-y-6 lg:space-y-4">
-        {ghostPosts.map(post => (
+        {posts.map(post => (
           <li key={post.id}>
-            <NextLink
-              href={{
-                pathname: '/posts/[...slug]',
-                query: { slug: post.slug },
-              }}
-              passHref
-              scroll={false}
-            >
+            <PostLink slug={post.slug}>
               <a>
                 <h2>{post.title}</h2>
-                <span>
-                  {post.published_at &&
-                    formatDateTime(post.published_at, 'full-date-localized')}
-                </span>
+                <span>{post.publishedAt}</span>
               </a>
-            </NextLink>
+            </PostLink>
           </li>
-        ))}
-      </ul>
-      <h1 className="font-medium mb-4 text-3xl underline">Latest Posts</h1>
-      <ul className="flex flex-col mb-8 overflow-y-scroll p-2 space-y-6 lg:space-y-4">
-        {posts.map(({ frontMatter }) => (
-          <PostListItem
-            dateFormat="day-month"
-            frontMatter={frontMatter}
-            key={frontMatter.slug}
-          />
-        ))}
-      </ul>
-      <h1 className="font-medium mb-4 text-3xl underline">Archive</h1>
-      <ul className="flex flex-col mb-8 overflow-y-scroll p-2 space-y-6 lg:space-y-4">
-        {Object.keys(archive).map(year => (
-          <div key={year}>
-            <h2 className="font-medium mb-4 text-accent-magenta text-2xl">
-              {year}
-            </h2>
-            <ul className="flex flex-col space-y-6 lg:ml-4 lg:space-y-4">
-              {archive[year].map(({ frontMatter, source }) => (
-                <ArchivedPost frontMatter={frontMatter} key={source} />
-              ))}
-            </ul>
-          </div>
         ))}
       </ul>
     </AnimatedPage>
@@ -83,17 +35,12 @@ const Home: React.FC<Props> = ({ archive, ghostPosts, posts }) => {
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   try {
-    const posts = await getAllPostsFrontMatter()
-    const ghostPosts = await getGhostPosts()
+    const { pagination, posts } = await getPosts()
 
     return {
       props: {
-        archive: createArchivedPostMap(
-          filterPosts(posts, p => p.frontMatter.archived)
-        ),
-        ghostPosts,
-        // All posts for current year.
-        posts: filterPosts(posts, p => !p.frontMatter.archived),
+        pagination,
+        posts,
       },
     }
   } catch (error) {
