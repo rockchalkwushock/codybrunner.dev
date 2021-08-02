5b09c930b61fe890978701b6a06e6f77c5ecebe6
Cody Brunner <rockchalkwushock@icloud.com>
2021-08-02 17:22:46 -0500

    fix(ghost-cms): fixes internal formatting of slugs & return path to ghost-cms

--- components/PostLink.tsx
+++ components/PostLink.tsx
@@ -9,7 +9,7 @@ export const PostLink = React.forwardRef<HTMLAnchorElement, Props>(
   ({ children, onClick, slug, ...rest }, ref) => {
     return (
       <NextLink
-        href={{ pathname: '/blog/[slug]', query: { slug } }}
+        href={{ pathname: '/blog/[...slug]', query: { slug: slug.split('/') } }}
         passHref
         // We set this to false so we don't get the wonky behavior of NextLink scrolling
         // us to the top of the page and then framer-motion initiating the page transition.
